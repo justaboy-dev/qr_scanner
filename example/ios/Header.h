1fc234a7414e7e08e7135ff9d4edb02a80cae9af
Tan <tan@Tans-Mac.local>
2022-08-25 11:54:08 +0700

    Implement ios zoom

---
+++ example/ios/Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  MTBBarcodeScanner
+//
+//  Created by Tan on 24/08/2022.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ ios/Classes/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  MTBBarcodeScanner
+//
+//  Created by Tan on 24/08/2022.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "MTBBarcodeScanner.h"
+
+#endif /* Header_h */
